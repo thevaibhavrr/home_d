7fcddd2cf18db8d6000333f3ad4f09cd24da2b13
vaibhav_rathore <vaibahvrathorema@gmail.com>
2025-01-28 08:00:15 +0530

    new link

--- src/api/callApi.jsx
+++ src/api/callApi.jsx
@@ -22,7 +22,7 @@ export const makeApi = async (
       method,
       // url: `https://test.ritaz.in${endpoint}`, 
       // url: `https://new-food-backend-fdoa.onrender.com${endpoint}`,
-      url: `https://belivmart-user.onrender.com${endpoint}`,
+      url: `https://belivmart-backend.onrender.com${endpoint}`,
       // url:"http://localhost:5008"+endpoint,
       headers,
       data 
